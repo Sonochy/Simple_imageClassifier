410fd38f989d037bb2197501444770934ca65c73
TS <sono@wlan-napt-002.u-aizu.ac.jp>
2020-03-04 19:33:05 +0900

    resume argparse

--- train.py
+++ train.py
@@ -19,7 +19,7 @@ SLASH = 0.2 # percentage of test(validation) data
 # BATCH:勾配更新毎のサンプル数を示す
 BATCH_SIZE = 20
 # EPOCH:訓練データ配列の反復回数,一度に処理する学習回数
-#EPOCH = 20
+EPOCH = 20
 # Patiemce:EarlyStopping参照
 PATIENCE = 100
 MONITOR = 'val_acc'
@@ -40,7 +40,7 @@ def parse_args():
     parser.add_argument('--data', dest='data_dir', default='data')
     parser.add_argument('--list', dest='list_dir', default='list')
     parser.add_argument('--model_name', dest='model_name', default = date_str)
-    parser.add_argument('--epoch', dest='epoch', default = 20)
+    # parser.add_argument('--epoch', dest='epoch', default = 20)
     args = parser.parse_args()
     return args
 
